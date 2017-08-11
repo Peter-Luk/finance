f4859696cbf764848bcd7d391843fdf312d7e412
Peter-Luk <lukwyuen@gmail.com>
2017-08-11 14:27:01 +0800

    Enhancement

--- iolimits.py
+++ iolimits.py
@@ -1,29 +1,30 @@
-him = getattr(__import__('handy'),'him')
+him = getattr(__import__('handy'), 'him')
 __ = him({'utilities':('mtf',), 'bt':('LF',), 'os':('linesep',)})
-for _ in list(__.keys()):exec("%s=__['%s']"%(_,_))
+for _ in list(__.keys()): exec("%s=__['%s']" % (_,_))
 fit = 'mhi'
 pf = mtf(fit)
-mpd = getattr(lf(pf),'fp')
-print('%s: (latest @ %s)'%(pf,mpd.trade_day[-1]))
-xd = getattr(mpd,'xfinder')('d')
-xr = getattr(mpd,'xfinder')('r')
+mpd = getattr(lf(pf), 'fp')
+print('%s: (latest @ %s)' % (pf, mpd.trade_day[-1]))
+xd = getattr(mpd, 'xfinder')('d')
+xr = getattr(mpd, 'xfinder')('r')
+ai, ar, mos = getattr(mpd, 'fdc')('b'), getattr(mpd, 'fdc')('i'), getattr(mpd, 'ltdmos')('a')
 try:
     dtxd = xd.transpose().to_dict()
     for _ in list(dtxd.keys()):
-        print("%s: delta @ %i" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta']))
+        print("%s: delta @ %i (mean %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], ar['Delta'].mean()))
 except:pass
 try:
     dtxr = xr.transpose().to_dict()
     for _ in list(dtxr.keys()):
-        print("%s: RSI @ %.3f" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI']))
+        print("%s: RSI @ %.3f (mean %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], ai['RSI'].mean()))
 except:pass
-ar, mos = getattr(mpd,'fdc')('b'), getattr(mpd,'ltdmos')('a')
+ar, mos = getattr(mpd, 'fdc')('b'), getattr(mpd, 'ltdmos')('a')
 lv, vm, vs = ar['Volume'].values[-1], ar['Volume'].mean(), ar['Volume'].std()
 print("%sVolume over mean: %.2f%%" % (linesep, lv / vm* 100.))
 print("Volume over (mean + std): %.2f%%" % (lv / (vm +vs) * 100.))
 lc, cs = ar['Close'].values[-1], ar['Close'].std()
 il = list(filter(lambda _:(_ > lc - cs) and (_ < lc + cs), mos))
-ol = list(filter(lambda _:(_ < lc -cs) or (_ > lc + cs), mos))
+ol = list(filter(lambda _:(_ < lc - cs) or (_ > lc + cs), mos))
 print('%sLast Close: %i' % (linesep, lc))
 print('%sWithin statistical range:' % linesep, il)
 ml = list(filter(lambda _:_ > lc, ol))
