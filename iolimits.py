a242130d0e542ac27e2c239d7018cf9b973e6069
Peter-Luk <peterluk@programmer.net>
2017-08-12 17:50:25 +0800

    Preemptive amendment of no extreme case - Part 2

--- iolimits.py
+++ iolimits.py
@@ -7,15 +7,16 @@ def an(*args, **kwargs):
     if 'code' in list(kwargs.keys()):
         if kwargs['code'] in waf(): pf = kwargs['code']
     mpd = getattr(lf(pf), 'fp')
-    print('%s: (latest @ %s)%sExtreme case:' % (pf, mpd.trade_day[-1], linesep))
+    print('%s: (latest @ %s)' % (pf, mpd.trade_day[-1]))
     try:
         ai = getattr(mpd, 'fdc')('i')
         xr = getattr(mpd, 'xfinder')('r')
         rm, rs = ai['RSI'].mean(), ai['RSI'].std()
         dtxr = xr.transpose().to_dict()
         if len(dtxr.keys()):
+            print('RSI extreme case:')
             for _ in list(dtxr.keys()):
-                print("%s: RSI @ %.3f (%.3f - %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], rm - rs, rm + rs))
+                print("%s: %.3f (%.3f - %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], rm - rs, rm + rs))
     except:pass
     try:
         mos = getattr(mpd, 'ltdmos')('a')
@@ -24,8 +25,9 @@ def an(*args, **kwargs):
         dm, ds = ar['Delta'].mean(), ar['Delta'].std()
         dtxd = xd.transpose().to_dict()
         if len(dtxd.keys()):
+            print('Delta extreme case:')
             for _ in list(dtxd.keys()):
-                print("%s: delta @ %i (%i - %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], dm - ds, dm + ds))
+                print("%s: %i (%i - %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], dm - ds, dm + ds))
         lv, vm, vs = ar['Volume'].values[-1], ar['Volume'].mean(), ar['Volume'].std()
         lc, cs = ar['Close'].values[-1], ar['Close'].std()
         print('%sLatest Close: %i' % (linesep, lc))
