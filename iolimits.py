424b71e49091d7d9031341d1e505981b2dad5da3
Peter-Luk <peterluk@programmer.net>
2017-08-12 12:33:21 +0800

    include more statistical info

--- iolimits.py
+++ iolimits.py
@@ -6,19 +6,27 @@ def an(*args, **kwargs):
     if 'futures_type' in list(kwargs.keys()): fit = kwargs['futures_type']
     pf = mtf(fit)
     mpd = getattr(lf(pf), 'fp')
-    ai, ar, mos = getattr(mpd, 'fdc')('i'), getattr(mpd, 'fdc')('b'), getattr(mpd, 'ltdmos')('a')
+#    ai, ar, mos = getattr(mpd, 'fdc')('i'), getattr(mpd, 'fdc')('b'), getattr(mpd, 'ltdmos')('a')
+#    ai = getattr(mpd, 'fdc')('i')
     print('%s: (latest @ %s)' % (pf, mpd.trade_day[-1]))
-    xd = getattr(mpd, 'xfinder')('d')
-    xr = getattr(mpd, 'xfinder')('r')
+#    xd = getattr(mpd, 'xfinder')('d')
+#    xr = getattr(mpd, 'xfinder')('r')
     try:
+        mos = getattr(mpd, 'ltdmos')('a')
+        ar = getattr(mpd, 'fdc')('b')
+        xd = getattr(mpd, 'xfinder')('d')
+        dm, ds = ar['Delta'].mean(), ar['Delta'].std()
         dtxd = xd.transpose().to_dict()
         for _ in list(dtxd.keys()):
-            print("%s: delta @ %i (mean %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], ar['Delta'].mean()))
+            print("%s: delta @ %i (%i - %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], dm - ds, dm + ds))
     except:pass
     try:
+        ai = getattr(mpd, 'fdc')('i')
+        xr = getattr(mpd, 'xfinder')('r')
+        rm, rs = ai['RSI'].mean(), ai['RSI'].std()
         dtxr = xr.transpose().to_dict()
         for _ in list(dtxr.keys()):
-            print("%s: RSI @ %.3f (mean %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], ai['RSI'].mean()))
+            print("%s: RSI @ %.3f (%.3f - %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], rm - rs, rm + rs))
     except:pass
     lv, vm, vs = ar['Volume'].values[-1], ar['Volume'].mean(), ar['Volume'].std()
     print("%sVolume over mean: %.2f%%" % (linesep, lv / vm* 100.))
@@ -32,4 +40,4 @@ def an(*args, **kwargs):
     csl = list(filter(lambda _:_ not in ml, ol))
     if ml: print('%sMoon shot:' % linesep, ml)
     if csl: print('China syndrome:', csl)
-an('hsi')
+an('mhi')
