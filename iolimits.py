561040df26c213a6d2b049f31d6cd05d3620ed1d
Peter-Luk <peterluk@programmer.net>
2017-08-12 12:51:59 +0800

    re-arrange display format

--- iolimits.py
+++ iolimits.py
@@ -8,15 +8,6 @@ def an(*args, **kwargs):
     mpd = getattr(lf(pf), 'fp')
     print('%s: (latest @ %s)' % (pf, mpd.trade_day[-1]))
     try:
-        mos = getattr(mpd, 'ltdmos')('a')
-        ar = getattr(mpd, 'fdc')('b')
-        xd = getattr(mpd, 'xfinder')('d')
-        dm, ds = ar['Delta'].mean(), ar['Delta'].std()
-        dtxd = xd.transpose().to_dict()
-        for _ in list(dtxd.keys()):
-            print("%s: delta @ %i (%i - %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], dm - ds, dm + ds))
-    except:pass
-    try:
         ai = getattr(mpd, 'fdc')('i')
         xr = getattr(mpd, 'xfinder')('r')
         rm, rs = ai['RSI'].mean(), ai['RSI'].std()
@@ -24,16 +15,25 @@ def an(*args, **kwargs):
         for _ in list(dtxr.keys()):
             print("%s: RSI @ %.3f (%.3f - %.3f)" % (dtxr[_]['Date'].strftime('%d-%m-%Y'), dtxr[_]['RSI'], rm - rs, rm + rs))
     except:pass
-    lv, vm, vs = ar['Volume'].values[-1], ar['Volume'].mean(), ar['Volume'].std()
-    print("%sVolume over mean: %.2f%%" % (linesep, lv / vm* 100.))
-    print("Volume over (mean + std): %.2f%%" % (lv / (vm +vs) * 100.))
-    lc, cs = ar['Close'].values[-1], ar['Close'].std()
-    il = list(filter(lambda _:(_ > lc - cs) and (_ < lc + cs), mos))
-    ol = list(filter(lambda _:(_ < lc - cs) or (_ > lc + cs), mos))
-    print('%sLast Close: %i' % (linesep, lc))
-    print('%sWithin statistical range:' % linesep, il)
-    ml = list(filter(lambda _:_ > lc, ol))
-    csl = list(filter(lambda _:_ not in ml, ol))
-    if ml: print('%sMoon shot:' % linesep, ml)
-    if csl: print('China syndrome:', csl)
+    try:
+        mos = getattr(mpd, 'ltdmos')('a')
+        ar = getattr(mpd, 'fdc')('b')
+        xd = getattr(mpd, 'xfinder')('d')
+        dm, ds = ar['Delta'].mean(), ar['Delta'].std()
+        dtxd = xd.transpose().to_dict()
+        for _ in list(dtxd.keys()):
+            print("%s: delta @ %i (%i - %i)" % (dtxd[_]['Date'].strftime('%d-%m-%Y'), dtxd[_]['Delta'], dm - ds, dm + ds))
+        lv, vm, vs = ar['Volume'].values[-1], ar['Volume'].mean(), ar['Volume'].std()
+        print("%sVolume over mean: %.2f%%" % (linesep, lv / vm* 100.))
+        print("Volume over (mean + std): %.2f%%" % (lv / (vm +vs) * 100.))
+        lc, cs = ar['Close'].values[-1], ar['Close'].std()
+        il = list(filter(lambda _:(_ > lc - cs) and (_ < lc + cs), mos))
+        ol = list(filter(lambda _:(_ < lc - cs) or (_ > lc + cs), mos))
+        print('%sLast Close: %i' % (linesep, lc))
+        print('%sWithin statistical range:' % linesep, il)
+        ml = list(filter(lambda _:_ > lc, ol))
+        csl = list(filter(lambda _:_ not in ml, ol))
+        if ml: print('%sMoon shot:' % linesep, ml)
+        if csl: print('China syndrome:', csl)
+    except:pass
 an('mhi')
