0486f0e534c0db2ae114f8da8f0fa9b216458fd1
Peter-Luk <peterluk@programmer.net>
2017-08-13 08:47:52 +0800

    Attempt to catch all error cases - Part 1

--- iolimits.py
+++ iolimits.py
@@ -5,16 +5,19 @@ for _ in list(__.keys()): exec("%s=__['%s']" % (_,_))
 if __name__ == "__main__":
     confirm = 'Y'
     while confirm.upper() != 'N':
+        code = ''
         if version_info.major == 2: ct = raw_input("Futures (C)ode/(T)ype: ")
         if version_info.major == 3: ct = input("Futures (C)ode/(T)ype: ")
         if ct.upper() == 'C':
             if version_info.major == 2: contract = raw_input('Contract: ')
             if version_info.major == 3: contract = input('Contract: ')
-            if contract.upper() in waf(): pi(contract.upper()).ds()
+            if contract.upper() in waf(): code = contract.upper()
         elif ct.upper() == 'T':
             if version_info.major == 2: tp = raw_input('Type (H)SI/(M)HI: ')
             if version_info.major == 3: tp = input('Type (H)SI/(M)HI: ')
-            if tp.upper() == 'H': pi(mtf('hsi')).ds()
-            elif tp.upper() == 'M': pi(mtf('mhi')).ds()
+            if tp.upper() == 'H': code = mtf('hsi')
+            elif tp.upper() == 'M': code = mtf('mhi')
+        try: pi(code).ds()
+        except: print("Only for local futures (current or next calender) month")
         if version_info.major == 2: confirm = raw_input("Proceed (Y)es/(N)o: ")
         if version_info.major == 3: confirm = input("Proceed (Y)es/(N)o: ")
