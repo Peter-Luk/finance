320058ef467e96744299a4c4851d2b49930dbc26
Peter-Luk <peterluk@programmer.net>
2017-08-20 17:45:37 +0800

    Unsuccessful attend on multiprocessing

--- iolimits.py
+++ iolimits.py
@@ -1,9 +1,21 @@
 him = getattr(__import__('handy'), 'him')
-__ = him([{'utilities':('mtf','waf'), 'pt_2':('PI',), 'sys':('version_info',)},({'multiprocessing':()}, "alias='mp'")])
+__ = him([{'utilities':('mtf','waf'), 'pt_2':('PI',), 'sys':('version_info',)},({'multiprocessing':('Queue','Process','Pool')}, "case='capitalize'")])
 for _ in list(__.keys()): exec("%s=__['%s']" % (_,_))
+
+# def f(x):
+    # q.put(pi(x).ds(programmatic=False))
 
 if __name__ == "__main__":
     confirm = 'Y'
+    # dmp, q = {}, Queue()
+    # for _ in waf():
+        # p = Process(target=f, name=_, args=(_,))
+        # p.start()
+        # dmp[_] = q.get()
+        # p.join()
+    # p = Pool(4)
+    # res = p.map(f, waf())
+
     while confirm.upper() != 'N':
         code = ''
         if version_info.major == 2: ct = raw_input("Futures (C)ode/(T)ype: ")
@@ -18,6 +30,8 @@ if __name__ == "__main__":
             if tp.upper() == 'H': code = mtf('hsi')
             elif tp.upper() == 'M': code = mtf('mhi')
         try: print(pi(code).ds(programmatic=False))
+        # try: print(dmp[code])
+        # try: print(res[waf().index(code)])
         except: print("Only for local futures (current or next calender) month")
         if version_info.major == 2: confirm = raw_input("Proceed (Y)es/(N)o: ")
         if version_info.major == 3: confirm = input("Proceed (Y)es/(N)o: ")
