601eba3b7aa0a34d191512b5cd1f13948fcd2e48
Peter-Luk <peterluk@programmer.net>
2017-09-25 23:12:18 +0800

    refine from datetime.datetime type to datetime.date

--- et.py
+++ et.py
@@ -20,13 +20,12 @@ class Equities(object):
         del self.code, self.digits, self.__data, self.trade_date, self.latest, self.close
 
     def get(self, *args, **kwargs):
-        programmatic, digits = True, 2
+        digits = 2
         if 'code' in list(kwargs.keys()): self.code = kwargs['code']
         if args: code = args[0]
         if len(args) > 1: digits = int(args[1])
         if 'code' in list(kwargs.keys()): code = kwargs['code']
         if 'digits' in list(kwargs.keys()): digits = int(kwargs['digits'])
-        if 'programmatic' in list(kwargs.keys()): programmatic = kwargs['programmatic']
         tmp = open(filepath('.'.join((code, 'csv')), subpath='csv'))
         data = [_[:-1] for _ in tmp.readlines()]
         tmp.close()
@@ -35,7 +34,7 @@ class Equities(object):
         while i < len(data):
             tmp = data[i].split(',')
             try:
-                tmp[0] = datetime.strptime(tmp[0], '%Y-%m-%d')
+                tmp[0] = datetime.strptime(tmp[0], '%Y-%m-%d').date()
                 tmp[-1] = int(tmp[-1])
                 j = 1
                 while j < len(tmp) - 1:
@@ -46,26 +45,23 @@ class Equities(object):
             i += 1
         el  = ['Adj Close']
         rfl = [_ for _ in fields if _ not in el]
-        tmp = fields, values
-        if programmatic == True:
-            i, tmp  = 0, []
-            while i < len(values):
-                hdr = {}
-                for _ in rfl:
-                    j = fields.index(_)
-                    hdr[fields[j]] = values[i][j]
-                tmp.append(hdr)
-                i += 1
+        i, tmp  = 0, []
+        while i < len(values):
+            hdr = {}
+            for _ in rfl:
+                j = fields.index(_)
+                hdr[fields[j]] = values[i][j]
+            tmp.append(hdr)
+            i += 1
         return tmp
 
     def put(self, *args, **kwargs):
         data_type = 'sql'
         if args: table_name = args[0]
         if len(args) > 1: data_type = args[1]
-        fields, values = self.get(self.code, programmatic=False)
-        el = ['Adj Close']
-        rfl = [_ for _ in fields if _ not in el]
-        cp = fields.index('Date')
-        # exist = connect(
-        sqstr = "INSERT INTO %s (%s) VALUES" % (table_name, ','.join(rfl))
-
+        if 'name' in list(kwargs.keys()): table_name = kwargs['name']
+        if 'type' in list(kwargs.keys()): data_type = kwargs['type']
+        i = 0
+        while i < len(self.__data):
+            sqstr = "INSERT INTO %s (%s) VALUES" % (table_name, ','.join(list(self.__data[i].keys())))
+            i += 1
