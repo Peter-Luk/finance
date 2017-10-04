98604dbc17ac0f8afdfe6bfdb8defb8a908dace0
Peter-Luk <peterluk@programmer.net>
2017-10-04 17:57:47 +0800

    correct closing sqlite3 connection

--- c2lite3e.py
+++ c2lite3e.py
@@ -17,9 +17,8 @@ class Equities(object):
         self.__stored_data = conn.cursor().execute("SELECT * FROM %s WHERE eid=%i ORDER BY date ASC" % (db_table, int(self.code.split('.')[0]))).fetchall()
 
     def __del__(self):
-        conn.close()
-        conn = self.code = self.digits = self.__data = self.__stored_data = None
-        del conn, self.code, self.digits, self.__data, self.__stored_data
+        self.code = self.digits = self.__data = self.__stored_data = None
+        del self.code, self.digits, self.__data, self.__stored_data
 
     def get(self, *args, **kwargs):
         digits = 2
