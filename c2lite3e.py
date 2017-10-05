efaf9ca00ca9e19d87828f359523687628410e8e
Peter-Luk <peterluk@programmer.net>
2017-10-05 14:48:04 +0800

    barebone transfer data from csv to sqlite

--- c2lite3e.py
+++ c2lite3e.py
@@ -6,28 +6,23 @@ db_name, db_table = 'Securities', 'records'
 
 class Equities(object):
     def __init__(self, *args, **kwargs):
-        self.digits = 4
         if args: self.code = args[0]
-        if len(args) > 1: self.digits = int(args[1])
         if 'code' in list(kwargs.keys()): self.code = kwargs['code']
-        if 'digits' in list(kwargs.keys()): self.digits = int(kwargs['digits'])
         self.conn = lite.connect(filepath(db_name))
         self.conn.row_factory = lite.Row
-        self.__data = self.get(self.code, self.digits)
+        self.fields, self.values = [], []
+        self.__data = self.get(self.code)
         self.__stored_data = self.conn.cursor().execute("SELECT * FROM %s WHERE eid=%i ORDER BY date ASC" % (db_table, int(self.code.split('.')[0]))).fetchall()
 
     def __del__(self):
         self.conn.close()
-        self.conn = self.code = self.digits = self.__data = self.__stored_data = None
-        del self.conn, self.code, self.digits, self.__data, self.__stored_data
+        self.conn = self.fields = self.values = self.code = self.__data = self.__stored_data = None
+        del self.fields, self.values, self.conn, self.code, self.__data, self.__stored_data
 
     def get(self, *args, **kwargs):
-        digits = 2
         if 'code' in list(kwargs.keys()): self.code = kwargs['code']
         if args: code = args[0]
-        if len(args) > 1: digits = int(args[1])
         if 'code' in list(kwargs.keys()): code = kwargs['code']
-        if 'digits' in list(kwargs.keys()): digits = int(kwargs['digits'])
         tmp = open(filepath('.'.join((code, 'csv')), subpath='csv'))
         data = [_[:-1] for _ in tmp.readlines()]
         tmp.close()
@@ -40,48 +35,49 @@ class Equities(object):
                 tmp[-1] = int(tmp[-1])
                 j = 1
                 while j < len(tmp) - 1:
-                    tmp[j] = round(float(tmp[j]), digits)
+                    tmp[j] = float(tmp[j])
                     j += 1
                 values.append(tmp)
             except: pass
             i += 1
         el  = ['Adj Close']
-        rfl = [_ for _ in fields if _ not in el]
+        self.fields.extend([_ for _ in fields if _ not in el])
         i, tmp  = 0, []
         while i < len(values):
+            self.values.append(values[i])
             hdr = {}
-            for _ in rfl:
+            for _ in self.fields:
                 j = fields.index(_)
-                hdr[fields[j]] = values[i][j]
+                hdr[fields[j].lower()] = values[i][j]
             tmp.append(hdr)
             i += 1
         return tmp
 
     def store(self, *args, **kwargs):
-        table_name = db_table
+        eid, table_name = int(self.code.split('.')[0]), db_table
         if args: table_name = args[0]
         if 'name' in list(kwargs.keys()): table_name = kwargs['name']
         i, sd, nd = 0, [datetime.strptime(_['date'], '%Y-%m-%d').date() for _ in self.__stored_data], []
+        fields, vh = ['eid'], []
+        fields.extend([_.lower() for _ in self.fields])
+        for j in range(len(fields)):
+            if fields[j] == 'date': vh.append("'%s'")
+            elif fields[j] in ('volume', 'eid'): vh.append('%i')
+            else: vh.append('%f')
+        sqlstr = "INSERT INTO %s (%s) VALUES (%s)" % (table_name, ','.join(fields), ','.join(vh))
         while i < len(self.__data):
             _ = self.__data[i]
-            if _['Date'] not in sd: nd.append(_)
+            if _['date'] not in sd: nd.append(_)
             i += 1
-        i = 0
+        i, vs = 0, []
         while i < len(nd):
-            k, v, s = [_.lower() for _ in list(nd[i].keys())], list(nd[i].values()), []
-            for j in range(len(k)):
-                if k[j] == 'date':
+            v = [eid]
+            v.extend([_ for _ in list(nd[i].values())])
+            for j in range(len(fields)):
+                if fields[j] == 'date':
                     v[j] = v[j].strftime('%Y-%m-%d')
-                    s.append("'%s'")
-                elif k[j] == 'volume': s.append('%i')
-                else: s.append('%f')
-            k.append('eid')
-            v.append(int(self.code.split('.')[0]))
-            s.append('%i')
-            sqlstr = "INSERT INTO %s (%s) VALUES (" + ','.join(s) + ")"
-            tl = [table_name, ','.join(k)]
-            tl.extend(v)
-            self.conn.cursor().execute(sqlstr % tuple(tl))
+            vs.append(tuple(v))
             i += 1
+        if vs: [self.conn.cursor().execute(sqlstr % _) for _ in vs]
         self.conn.commit()
         return i
