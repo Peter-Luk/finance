491eaebb8a9301c307d1e0b8ac0283a1d979a2b0
Peter Luk <peterluk@programmer.net>
2022-07-20 20:45:41 +0800

    Wednesday, July 20, 2022 PM08:45:41 HKT

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
-from pathlib import os
 from .fat import Futures
 from .fat import Equity
 from .pa import Person
-# from .utilities import PYTHON_PATH
 
-__all__ = ['fapi', 'asplay', 'utilities', 'pref']
+__all__ = ['utilities', 'pref']
