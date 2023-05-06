8c5e9d96c226b8e4fe297ff576423ddf6640aaea
Peter Luk <peterluk@programmer.net>
2023-05-06 21:47:31 +0800

    Saturday, May 06, 2023 PM09:47:31 HKT

--- finaux.py
+++ finaux.py
@@ -1,17 +1,19 @@
 import numpy as np
 from sys import platform
+from finance.utilities import conditional_decorator
 
 if platform == 'win32':
     use_numba = False
 # else:
 try:
-    import numba as nb
+    from numba import jit
+    # import numba as nb
     use_numba = True
 except Exception:
     use_numba = False
 
-
-def _stepper(period, x):
+@conditional_decorator(jit, use_numba)
+def stepper(period, x):
     y = np.empty(len(x))
     y[:] = np.nan
     i = 0
@@ -22,8 +24,49 @@ def _stepper(period, x):
             y[i] = x[:i].mean()
         i += 1
     return y
+"""def _stepper(period, x):
+    y = np.empty(len(x))
+    y[:] = np.nan
+    i = 0
+    while i < len(x):
+        if i > period:
+            y[i] = (y[i - 1] * (period - 1) + x[i]) / period
+        elif i == period:
+            y[i] = x[:i].mean()
+        i += 1
+    return y
+"""
 
-
+@conditional_decorator(jit, use_numba)
+def roundup(value):
+    if np.isnan(value) or not value > 0:
+        hdr = np.nan
+    _ = np.floor(np.log10(value))
+    __ = np.int32(value / 10 ** (_ - 1))
+    if _ < 0:
+        if __ < 25:
+            hdr = np.round(value, 3)
+        if __ < 50:
+            hdr = np.round(value * 2, 2) / 2
+        hdr = np.round(value, 2)
+    if _ == 0:
+        hdr = np.round(value, 2)
+    if _ > 3:
+        hdr = np.round(value, 0)
+    if _ > 1:
+        if __ < 20:
+            hdr = np.round(value, 1)
+        if __ < 50:
+            hdr = np.round(value * 5, 0) / 5
+        hdr = np.round(value * 2, 0) / 2
+    if _ > 0:
+        if __ < 10:
+            hdr = np.round(value, 2)
+        if __ < 20:
+            hdr = np.round(value * 5, 1) / 5
+        hdr = np.round(value * 2, 1) / 2
+    return hdr
+"""
 def _roundup(value):
     if np.isnan(value) or not value > 0:
         hdr = np.nan
@@ -64,3 +107,4 @@ else:
     print('Falling back to python')
     stepper = _stepper
     roundup = _roundup
+"""
