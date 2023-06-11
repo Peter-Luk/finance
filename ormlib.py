0469789f8a454ea9340324a4663c0684d2cdb501
Peter Luk <peterluk@programmer.net>
2023-06-11 18:59:53 +0800

    Sunday, June 11, 2023 PM06:59:53 HKT

--- ormlib.py
+++ ormlib.py
@@ -1,8 +1,55 @@
 import datetime
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, Date, Time, String, text
+from pathlib import os, sys
+from sqlalchemy import create_engine, Column, Integer, Date, Time, String, text
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from utilities import filepath
 from pref import fields
 Base = declarative_base()
+
+
+def filepath(*args, **kwargs):
+    name, file_type, data_path = args[0], 'data', 'sqlite3'
+    if 'type' in list(kwargs.keys()):
+        file_type = kwargs['type']
+    if 'subpath' in list(kwargs.keys()):
+        data_path = kwargs['subpath']
+    if sys.platform == 'win32':
+        if sys.version_info.major > 2 and sys.version_info.minor > 3:
+            return os.sep.join((os.environ.get('HOMEPATH'), file_type, data_path, name))
+        else:
+            file_path = os.sep.join((os.environ.get('HOMEPATH'), file_type, data_path))
+            _ = os.sep.join((file_path, name))
+            return _ if os.path.exists(_) else False
+    # if platform == 'linux-armv7l':file_drive, file_path = '', sep.join(('mnt', 'sdcard', file_type, data_path))
+    if sys.platform in ('linux', 'linux2'):
+        if sys.version_info.major > 2 and sys.version_info.minor > 3:
+            if os.environ.get('EXTERNAL_STORAGE'):
+                _ = os.sep.join((
+                    os.environ.get('HOME'), 'storage', 'external-1', file_type,
+                    data_path, name))
+                if os.path.exists(_):
+                    return _
+            _ = os.sep.join((os.environ.get('HOME'), file_type, data_path, name))
+            return _ if os.path.exists(_) else False
+        else:
+            place = 'shared'
+            if os.environ.get('ACTUAL_HOME'):
+                file_path = os.sep.join((os.environ.get('HOME'), file_type, data_path))
+            elif os.environ.get('EXTERNAL_STORAGE') and ('/' in os.environ['EXTERNAL_STORAGE']):
+                place = 'external-1'
+                file_path = os.sep.join(
+                    (os.environ.get('HOME'), 'storage', place, file_type, data_path))
+            _ = os.sep.join((file_path, name))
+            return _ if os.path.exists(_) else False
+    else:
+        path_holder_list = ['..', file_type, data_path]
+        path_holder = os.sep.join(path_holder_list)
+        if os.path.isfile(path_holder):
+            return os.path.abspath(path_holder)
+        else:
+            path_holder = os.sep.join(['..', '..'] + path_holder_list)
+            return os.path.abspath(path_holder) if os.path.isfile(path_holder) else False
 
 
 class Securities(Base):
@@ -51,3 +98,15 @@ class Subject(Base):
     dia = Column(Integer)
     pulse = Column(Integer)
     remarks = Column(String)
+
+
+class Session(object):
+    def __init__(self, db_name: str, sync: bool = True):
+        db_file = filepath(db_name)
+        if os.path.isfile(db_file):
+            if sync:
+                self.engine = create_engine(f'sqlite:///{db_file}')
+                self.session = sessionmaker(self.engine, expire_on_commit=False)
+            else:
+                self.engine = create_async_engine(f'sqlite+aiosqlite:///{db_file}')
+                self.session = sessionmaker(self.engine, expire_on_commit=False, class_=AsyncSession)
