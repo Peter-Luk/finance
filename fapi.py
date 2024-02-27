0b40fc826c1b0dacdb30a6aef5ca7e4694d370d2
Peter Luk <peterluk@programmer.net>
2024-02-28 05:46:04 +0800

    Wednesday, February 28, 2024 AM05:46:04 HKT

--- fapi.py
+++ fapi.py
@@ -15,9 +15,42 @@ def ind_status(b:float, i:float) -> str:
     return  '+' if b > i else '-' if b < i else '='
 
 
+def _processor(data) -> dict:
+    ind_list: list = ['kama', 'sar', 'rsi', 'obv', 'kc']
+    data_close = data._Equity__data.Close
+    d0 = data.date
+    result = {f'{d0:%d-%m-%Y}': f'{data.change:+.2%}'}
+    result['close'] = round(data.close, 2)
+    for _ in ind_list:
+        val = eval(f'data.{_}()')
+        res = {'value': round(val.loc[d0], 3)}
+        if _ == 'rsi':
+            res['change'] = round(data.rsi().diff().loc[d0], 3)
+        elif _ == 'kc':
+            # val = eval(f'data.{_}()')
+            res = {}
+            res['value'] = [round(val.Upper.loc[d0], 3),
+                    round(val.Lower.loc[d0], 3)]
+            res['change'] = [round(val.Upper.diff().loc[d0], 3),
+                    round(val.Lower.diff().loc[d0], 3)]
+            tsh = (data_close - val.Upper).diff()
+            tsl = (data_close - val.Lower).diff()
+            res['delta'] = [ind_status(abs(tsh[-2]), abs(tsh[-1])),
+                    ind_status(abs(tsl[-2]), abs(tsl[-1]))]
+        elif _ == 'obv':
+            res['value'] = round(val.loc[d0], 1)
+            res['change'] = f'{val.pct_change().loc[d0]:+.2%}'
+        else:
+            ts = (data_close - val).diff()
+            res['delta'] = ind_status(abs(ts[-2]), abs(ts[-1]))
+        result[_] = res
+    return {data.yahoo_code: result}
+
+
 @app.get("/hkex/{code}")
 async def quote_hk(code: int):
     _ = await Equity(code, static=False)
+    # return _processor(_)
     return {_.yahoo_code: f'{_}'}
 
 
@@ -30,7 +63,8 @@ async def optinum_hk(code: int):
 @app.get("/tse/{code}")
 async def quote_tse(code: str):
     _ = await Equity(code, boarse='TSE')
-    return {_.yahoo_code: f'{_}'}
+    return _processor(_)
+    # return {_.yahoo_code: f'{_}'}
     # _ = Equity(tse_stock_code(code), exchange='TSE')
     # return {_.yahoo_code: f'{_}'}
 
@@ -47,21 +81,37 @@ async def optinum_tse(code: str):
 
 @app.get("/nyse/{code}")
 async def quote_nyse(code: str) -> dict:
-    ind_list: list = ['kama', 'sar', 'rsi']
     data = await Equity(code, boarse="NYSE")
-    data_close = data._Equity__data.Close
-    d0 = data.date
-    result = {f'{d0:%d-%m-%Y}': f'{data.change:+.2%}'}
-    result['close'] = round(data.close, 2)
-    for _ in ind_list:
-        res = {'value': eval(f'round(data.{_}().loc[d0], 3)')}
-        if _ == 'rsi':
-            res['change'] = round(data.rsi().diff().loc[d0], 3)
-        else:
-            ts = eval(f'(data_close - data.{_}()).diff()')
-            res['delta'] = ind_status(abs(ts[-2]), abs(ts[-1]))
-        result[_] = res
-    return {data.yahoo_code: result}
+    return _processor(data)
+    # ind_list: list = ['kama', 'sar', 'rsi', 'obv', 'kc']
+    # data_close = data._Equity__data.Close
+    # d0 = data.date
+    # result = {f'{d0:%d-%m-%Y}': f'{data.change:+.2%}'}
+    # result['close'] = round(data.close, 2)
+    # for _ in ind_list:
+    #     val = eval(f'data.{_}()')
+    #     res = {'value': round(val.loc[d0], 3)}
+    #     if _ == 'rsi':
+    #         res['change'] = round(data.rsi().diff().loc[d0], 3)
+    #     elif _ == 'kc':
+    #         # val = eval(f'data.{_}()')
+    #         res = {}
+    #         res['value'] = [round(val.Upper.loc[d0], 3),
+    #                 round(val.Lower.loc[d0], 3)]
+    #         res['change'] = [round(val.Upper.diff().loc[d0], 3),
+    #                 round(val.Lower.diff().loc[d0], 3)]
+    #         tsh = (data_close - val.Upper).diff()
+    #         tsl = (data_close - val.Lower).diff()
+    #         res['delta'] = [ind_status(abs(tsh[-2]), abs(tsh[-1])),
+    #                 ind_status(abs(tsl[-2]), abs(tsl[-1]))]
+    #     elif _ == 'obv':
+    #         res['value'] = round(val.loc[d0], 1)
+    #         res['change'] = f'{val.pct_change().loc[d0]:+.2%}'
+    #     else:
+    #         ts = (data_close - val).diff()
+    #         res['delta'] = ind_status(abs(ts[-2]), abs(ts[-1]))
+    #     result[_] = res
+    # return {data.yahoo_code: result}
 
 
 @app.get("/nyse/{code}/optinum")
