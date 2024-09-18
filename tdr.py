b019a1b9afddcffad36526da9698be3def54e8cd
Peter Luk <peterluk@programmer.net>
2024-09-18 21:31:13 +0800

    aarch64-unknown-linux-gnu

--- tdr.py
+++ tdr.py
@@ -1,59 +1,27 @@
-#!/data/data/com.termux/files/usr/bin/python
 """
+#!/data/data/com.termux/files/usr/bin/python
 #!/bin/python3
 """
 import asyncio
-import pandas as pd
 from tqdm import tqdm
-from asyncinit import asyncinit
 from benedict import benedict
 from pathlib import os
 from typing import Coroutine, Dict, List, Iterable, Final, Union
+from pstock import BarsMulti
 from utilities import getcode
 from fintools import hsirnd
-from asa import get_data
-# from asyahoo import get_data
 
 YAML_PREFERENCE: Final[str] = f'{os.sep}portfolio.yaml'
 clients = iter(('M213423', 'M241238', 'P724059', 'P772215', 'P851223'))
 
-@asyncinit
-class Equity():
-    async def __init__(
-            self,
-            code: Union[int, str],
-            boarse: str = 'HKEx',
-            capitalize: bool = True
-            ) -> Coroutine:
-        self.code = code
-        self.exchange = boarse
-        self.yahoo_code = getcode(self.code, self.exchange)
-        self.__capitalize = capitalize
-        await self.compose()
-
-    def __str__(self):
-        return f"{self.code} {hsirnd(self.close):0.2f}"
-
-    async def compose(self) -> pd.DataFrame:
-        self.__data = await get_data(self.code, self.exchange, self.__capitalize)
-        self.__data.index = self.__data.index.astype('datetime64[ns]')
-        self.date = self.__data.index[-1]
-        if self.__capitalize:
-            self.change = self.__data.Close.pct_change(fill_method=None).iloc[-1]
-            self.close = self.__data.Close.iloc[-1]
-        else:
-            self.change = self.__data.close.pct_change(fill_method=None).iloc[-1]
-            self.close = self.__data.close.iloc[-1]
 
 async def daily_close(
         client_no: str,
         boarse: str = 'HKEx') -> None:
-    result: List = []
     _: List = Portfolio(client_no, boarse)()
-    subject: Dict = dict(zip(_, [boarse for __ in _]))
-    for f in asyncio.as_completed([Equity(c, b) for c, b in tqdm(list(subject.items()))]):
-        holder = await f
-        result.append(f'{holder}')
+    _y = [getcode(__, boarse) for __ in tqdm(_)]
+    data = await BarsMulti.get(_y, period='5d', interval='1d')
+    result = [f'{i} {hsirnd(data[getcode(i, boarse)].df.close.iloc[-1]):0.2f}' for i in _]
     print(f"{client_no}\nClose price, {', '.join(result)}")
 
 async def main():
